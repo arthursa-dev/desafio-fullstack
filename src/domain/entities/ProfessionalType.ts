a7c922cbb599772352e5511e9506a3aca5258e3e
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-20 20:54:51 -0300

    refactor: change the name of class parameters

--- src/domain/entities/ProfessionalType.ts
+++ src/domain/entities/ProfessionalType.ts
@@ -1,4 +1,4 @@
-type Input = {
+type ProfessionalTypeProps = {
   id: string;
   description: string;
   situation: boolean;
@@ -13,7 +13,13 @@ export class ProfessionalType {
   createdAt: Date;
   updatedAt: Date;
 
-  constructor({ id, description, situation, createdAt, updatedAt }: Input) {
+  constructor({
+    id,
+    description,
+    situation,
+    createdAt,
+    updatedAt
+  }: ProfessionalTypeProps) {
     this.id = id;
     this.description = description;
     this.situation = situation;
