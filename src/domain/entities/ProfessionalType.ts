971d57f122ca2ee3023392d1af487da43b51f342
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-20 19:43:28 -0300

    feat: creates ProfessionalType entity

---
+++ src/domain/entities/ProfessionalType.ts
@@ -0,0 +1,23 @@
+type Input = {
+  id: string;
+  description: string;
+  situation: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class ProfessionalType {
+  id: string;
+  description: string;
+  situation: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor({ id, description, situation, createdAt, updatedAt }: Input) {
+    this.id = id;
+    this.description = description;
+    this.situation = situation;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
