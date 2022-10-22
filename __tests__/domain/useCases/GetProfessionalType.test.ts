8b1bde5f0b41d0b2ade984a4e06c7c5809277086
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-22 13:07:11 -0300

    test(GetProfessionalType): ensure CreateProfessionalType throw an error if ProfessionalTypeRepository throws

--- __tests__/domain/useCases/GetProfessionalType.test.ts
+++ __tests__/domain/useCases/GetProfessionalType.test.ts
@@ -28,4 +28,25 @@ describe('GetProfessionalType Use Case', () => {
 
     expect(professionalTypeRepository.id).toBe(input.id);
   });
+
+  it('should throw an error if ProfessionalTypeRepository throws', async () => {
+    class ProfessionalTypeRepositoryStub implements ProfessionalTypeRepository {
+      add(input: { description: string; situation: boolean; }): Promise<ProfessionalType> {
+        throw new Error("Method not implemented.");
+      }
+      get(input: { id: string; }): Promise<ProfessionalType | undefined> {
+        throw new Error();
+      }
+    }
+    const input = {
+      id: 'any_id',
+    };
+    const professionalTypeRepository = new ProfessionalTypeRepositoryStub();
+    const getProfessionalType = new GetProfessionalType(
+      professionalTypeRepository
+    );
+
+    expect(() => getProfessionalType.execute(input))
+      .rejects.toThrow();
+  });
 });
