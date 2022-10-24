e58bdac71f6942f85b4825659a90b46be7980844
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-23 22:39:36 -0300

    feat: create route to create a professional type

--- src/infra/repositories/db/ProfessionalTypeDBRepository.ts
+++ src/infra/repositories/db/ProfessionalTypeDBRepository.ts
@@ -9,9 +9,18 @@ export class ProfessionalTypeDBRepository implements ProfessionalTypeRepository 
     this.databaseConnection = databaseConnection;
   }
   
-  add(input: { description: string; situation: boolean; }): Promise<ProfessionalType> {
-    throw new Error("Method not implemented.");
+  async add({ description, situation }: { description: string; situation: boolean; }): Promise<ProfessionalType> {
+    const [result] = await this.databaseConnection.query(
+      "insert into professional_type (description, situation) values ($1, $2) returning *", [description, situation]);
+    return new ProfessionalType({
+      id: result.id,
+      description: result.description,
+      situation: result.situation,
+      createdAt: result.created_at,
+      updatedAt: result.updated_at,
+    });
   }
+
   async get({ id }: { id: string; }): Promise<ProfessionalType | undefined> {
     const [result] = await this.databaseConnection.query("select * from professional_type where id = $1", [id]);
     return new ProfessionalType({
@@ -22,9 +31,11 @@ export class ProfessionalTypeDBRepository implements ProfessionalTypeRepository 
       updatedAt: result.updated_at,
     });
   }
+
   update(input: { id: string; description?: string | undefined; situation?: boolean | undefined; }): Promise<ProfessionalType | undefined> {
     throw new Error("Method not implemented.");
   }
+
   async list(): Promise<ProfessionalType[]> {
     const result = await this.databaseConnection.query("select * from professional_type", []);
     return result.map((professionalTypeData: any) => new ProfessionalType({
--- src/main/routes/professionalType.routes.ts
+++ src/main/routes/professionalType.routes.ts
@@ -1,4 +1,5 @@
 import Router from 'express';
+import { CreateProfessionalType } from '../../application/useCases/CreateProfessionalType';
 import { GetProfessionalType } from '../../application/useCases/GetProfessionalType';
 import { ListProfessionalType } from '../../application/useCases/ListProfessionalType';
 import { PostgresDatabaseConnection } from '../../infra/db/postgresql/PostgresDatabaseConnection';
@@ -22,5 +23,14 @@ professionalTypeRouter.get('/:id', async (req, res) => {
     professionalTypeDBRepository
   );
   const result = await getProfessionalType.execute({ id });
+  return res.json(result);
+});
+
+professionalTypeRouter.post('/', async (req, res) => {
+  const { description, situation = true } = req.body;
+  const createProfessionalType = new CreateProfessionalType(
+    professionalTypeDBRepository
+  );
+  const result = await createProfessionalType.execute({ description, situation });
   return res.json(result);
 });
