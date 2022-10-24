d80b465291f21c0360d423072f3a9c4b9d2dc155
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-24 15:47:46 -0300

    test: creates Professional entity

---
+++ __tests__/domain/entities/Professional.test.ts
@@ -0,0 +1,28 @@
+import { Professional } from "../../../src/domain/entities/Professional";
+
+describe('Professional', () => {
+  it('should create a professional', () => {
+    const now = new Date();
+    const professional = new Professional({
+      id: 'valid_id',
+      name: 'name',
+      phone: '(99) 99999-9999',
+      email: 'valid_email@mail.com',
+      professionalType: 'valid_id',
+      situation: true,
+      createdAt: now,
+      updatedAt: now,
+    });
+
+    expect(professional).toEqual({
+      id: 'valid_id',
+      name: 'name',
+      phone: '(99) 99999-9999',
+      email: 'valid_email@mail.com',
+      professionalType: 'valid_id',
+      situation: true,
+      createdAt: now,
+      updatedAt: now,
+    });
+  });
+});
