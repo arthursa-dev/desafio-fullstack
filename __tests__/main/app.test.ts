387c1e0fadb07f80d88f9593493c9856166abb73
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-25 17:24:42 -0300

    test: refactor tests to clear and populate the database before and after each test

--- __tests__/main/app.test.ts
+++ __tests__/main/app.test.ts
@@ -1,66 +1,87 @@
 import request from 'supertest';
+import { PostgresDatabaseConnection } from '../../src/infra/db/postgresql/PostgresDatabaseConnection';
 import { app } from '../../src/main/app';
 
-describe.skip('/professional-type', () => {
-  // TODO: clear database before each test
+const databaseConnection = new PostgresDatabaseConnection();
+const professionalType = {
+  id: 'dd145fa8-7557-40cf-9973-044ec26cd9ff',
+  description: 'test description',
+  situation: false,
+}
+
+describe('/professional-type', () => {
+  beforeEach(async () => {
+    await databaseConnection.query('delete from professional_type', []);
+    await databaseConnection.query(
+      'insert into professional_type(id, description, situation) values ($1,$2,$3)',
+      [professionalType.id, professionalType.description, professionalType.situation]
+    );
+  });
+
+  afterAll(async () => {
+    await databaseConnection.query('delete from professional_type', []);
+  });
   
   it('GET /', async () => {
     const response = await request(app).get('/professional-type');
   
     expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
     expect(response.body).toContainEqual({
-      id: expect.any(String),
-      description: 'Professor',
-      situation: true,
+      id: professionalType.id,
+      description: professionalType.description,
+      situation: professionalType.situation,
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
   });
 
   it('GET /:id', async () => {
-    const response = await request(app).get('/professional-type/9b79242b-0d95-485d-aa29-847bcffb5985');
+    const response = await request(app).get(`/professional-type/${professionalType.id}`);
   
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      id: expect.any(String),
-      description: 'Cientista',
-      situation: true,
+      id: professionalType.id,
+      description: professionalType.description,
+      situation: professionalType.situation,
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
   });
 
   it('POST /', async () => {
+    const requestBody = {
+      description: 'new description',
+      situation: true,
+    }
     const response = await request(app)
       .post('/professional-type')
-      .send({
-        description: 'Médica',
-        situation: true,
-      });
+      .send(requestBody);
   
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
       id: expect.any(String),
-      description: 'Médica',
-      situation: true,
+      description: requestBody.description,
+      situation: requestBody.situation,
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
   });
 
   it('PUT /:id', async () => {
+    const requestBody = {
+      description: 'updated description',
+      situation: true,
+    }
     const response = await request(app)
-    .put('/professional-type/ac154cd0-6973-4950-b4cf-98ab46cdc40d')
-    .send({
-      description: 'Médica veterinária',
-      situation: false,
-    });
+    .put(`/professional-type/${professionalType.id}`)
+    .send(requestBody);
     
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      id: expect.any(String),
-      description: 'Médica veterinária',
-      situation: false,
+      id: professionalType.id,
+      description: requestBody.description,
+      situation: requestBody.situation,
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
