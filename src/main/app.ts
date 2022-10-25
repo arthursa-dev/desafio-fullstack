2eaee2b47ab80bc5284bd6214ddab31a7c53ec34
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-25 17:22:46 -0300

    chore: configure dotenv

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,3 +1,8 @@
+import * as dotenv from 'dotenv';
+import path from 'path';
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+const envPath = path.resolve(__dirname, `../../${envFile}`);
+dotenv.config({ path: envPath });
 import express from 'express';
 import { router } from './routes/index.routes';
 
