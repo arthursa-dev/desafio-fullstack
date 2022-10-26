7e6bc6ffa6a04289ab818c0f05a17bdfb44df094
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-25 22:17:50 -0300

    feat: create route to get professional by id

--- src/infra/repositories/db/ProfessionalDBRepository.ts
+++ src/infra/repositories/db/ProfessionalDBRepository.ts
@@ -13,8 +13,27 @@ export class ProfessionalDBRepository implements ProfessionalRepository {
     throw new Error("Method not implemented.");
   }
 
-  get(input: { id: string; }): Promise<Professional> {
-    throw new Error("Method not implemented.");
+  async get({ id }: { id: string; }): Promise<Professional> {
+    const [result] = await this.databaseConnection.query(
+      "select * from professional as p join (select id as ptId, description as ptDescription, situation as ptSituation, created_at as ptCreated_at, updated_at as ptUpdated_at from professional_type) pt on pt.ptId = p.professional_type where p.id = $1",
+      [id]
+    );
+    return new Professional({
+      id: result.id,
+      name: result.name,
+      phone: result.phone,
+      email: result.email,
+      professionalType: {
+        id: result.ptid,
+        description: result.ptdescription,
+        situation: result.ptsituation,
+        createdAt: result.ptcreated_at,
+        updatedAt: result.ptupdated_at,
+      },
+      situation: result.situation,
+      createdAt: result.created_at,
+      updatedAt: result.updated_at,
+    });
   }
 
   update(input: { id: string; name: string; phone: string; email: string; professionalType: string; situation: boolean; }): Promise<Professional> {
@@ -26,7 +45,6 @@ export class ProfessionalDBRepository implements ProfessionalRepository {
       "select * from professional as p join (select id as ptId, description as ptDescription, situation as ptSituation, created_at as ptCreated_at, updated_at as ptUpdated_at from professional_type) pt on pt.ptId = p.professional_type",
       []
     );
-    console.log("🚀 ~ file: ProfessionalDBRepository.ts ~ line 29 ~ ProfessionalDBRepository ~ list ~ result", result)
     return result.map((professionalData: any) => new Professional({
       id: professionalData.id,
       name: professionalData.name,
--- src/main/routes/professional.routes.ts
+++ src/main/routes/professional.routes.ts
@@ -1,4 +1,5 @@
 import Router from 'express';
+import { GetProfessional } from '../../application/useCases/GetProfessional';
 import { ListProfessional } from '../../application/useCases/ListProfessional';
 import { PostgresDatabaseConnection } from '../../infra/db/postgresql/PostgresDatabaseConnection';
 import { ProfessionalDBRepository } from '../../infra/repositories/db/ProfessionalDBRepository';
@@ -12,5 +13,14 @@ professionalRouter.get('/', async (req, res) => {
     professionalDBRepository
   );
   const result = await listProfessional.execute();
+  return res.json(result);
+});
+
+professionalRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const getProfessional = new GetProfessional(
+    professionalDBRepository
+  );
+  const result = await getProfessional.execute({ id });
   return res.json(result);
 });
