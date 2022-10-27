6070d4b2862b96378ee6fd6d5b99430ac162e3ef
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-27 13:56:24 -0300

    chore: use cors middleware

--- backend/src/main/app.ts
+++ backend/src/main/app.ts
@@ -3,10 +3,12 @@ import path from 'path';
 const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
 const envPath = path.resolve(__dirname, `../../${envFile}`);
 dotenv.config({ path: envPath });
+import cors from 'cors';
 import express from 'express';
 import { router } from './routes/index.routes';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/', router);
