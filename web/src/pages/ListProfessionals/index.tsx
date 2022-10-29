92026f8147a6bdef58088eea58a648aa3f69273d
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-29 16:54:33 -0300

    feat: add modal to update a selected professional

--- web/src/pages/ListProfessionals/index.tsx
+++ web/src/pages/ListProfessionals/index.tsx
@@ -1,63 +1,15 @@
-import { useEffect, useState } from "react";
-import { Checkbox, Table, Typography } from "antd";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Checkbox, Form, Modal, Table, Typography } from "antd";
 import { EditOutlined } from '@ant-design/icons';
 
-import { getProfessionals } from "../../services/getProfessionals";
+import { CreateProfessionalForm } from "../../components/CreateProfessionalForm";
 
-import type { ColumnsType } from 'antd/es/table';
+import { getProfessionals } from "../../services/getProfessionals";
+import { updateProfessional } from "../../services/updateProfessional";
+
 import { Professional } from "../../types";
 
-const { Text, Title } = Typography;
-
-interface DataType {
-  key: string;
-  name: string;
-  phone: string;
-  email: string;
-  professionalType: string;
-  situation: boolean
-}
-
-const columns: ColumnsType<DataType> = [
-  {
-    key: 'name',
-    title: 'Nome',
-    dataIndex: 'name',
-  },
-  {
-    key: 'phone',
-    title: 'Telefone',
-    dataIndex: 'phone',
-    responsive: ["sm"],
-  },
-  {
-    key: 'email',
-    title: 'E-mail',
-    dataIndex: 'email',
-    responsive: ["sm"],
-  },
-  {
-    key: 'professionalType',
-    title: 'Cargo',
-    dataIndex: 'professionalType',
-  },
-  {
-    key: 'situation',
-    title: 'Situação',
-    dataIndex: 'situation',
-    responsive: ["sm"],
-    render: (value: boolean) => (
-      <Checkbox checked={value} disabled={false} />
-    ),
-  },
-  {
-    title: '',
-    key: 'action',
-    render: () => (
-      <EditOutlined />
-    ),
-  },
-];
+const { Title } = Typography;
 
 function getProfessionalsDataSource(data: Professional[]) {
   return data.map((professional) => ({
@@ -70,8 +22,37 @@ function getProfessionalsDataSource(data: Professional[]) {
   }));
 }
 
+function findItemIndexBydId(professionals: Professional[], id: string) {
+  return professionals.findIndex((professional) => professional.id === id);
+}
+
 export function ListProfessionals() {
   const [professionals, setProfessionals] = useState<Professional[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [form] = Form.useForm();
+  const selectedProfessional = useRef<{
+    index: number;
+    data: Professional;
+  }>();
+  
+  const handleCancel = useCallback(() => setIsModalOpen(false), []);
+
+  const handleOk = useCallback(async () => {
+    setConfirmLoading(true);
+    form.submit();
+    const formData = form.getFieldsValue();
+    try {
+      await updateProfessional(selectedProfessional.current?.data.id!, formData);
+      const result = await getProfessionals();
+      setProfessionals(result)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsModalOpen(false);
+      setConfirmLoading(false);
+    }
+  }, []);
   
   useEffect(() => {
     getProfessionals()
@@ -83,10 +64,70 @@ export function ListProfessionals() {
       <Title>Lista de Profissionais</Title>
       <Table
         id="professionals-table"
-        columns={columns}
+        columns={[
+          {
+            key: 'name',
+            title: 'Nome',
+            dataIndex: 'name',
+          },
+          {
+            key: 'phone',
+            title: 'Telefone',
+            dataIndex: 'phone',
+            responsive: ["sm"],
+          },
+          {
+            key: 'email',
+            title: 'E-mail',
+            dataIndex: 'email',
+            responsive: ["sm"],
+          },
+          {
+            key: 'professionalType',
+            title: 'Cargo',
+            dataIndex: 'professionalType',
+          },
+          {
+            key: 'situation',
+            title: 'Situação',
+            dataIndex: 'situation',
+            responsive: ["sm"],
+            render: (value: boolean) => (
+              <Checkbox checked={value} disabled={false} />
+            ),
+          },
+          {
+            title: '',
+            key: 'action',
+            render: (value: { key: string }) => (
+              <EditOutlined
+                onClick={() => {
+                  const index = findItemIndexBydId(professionals, value.key);
+                  selectedProfessional.current = {
+                    index,
+                    data: professionals[index]
+                  };
+                  setIsModalOpen(true);
+                }}
+              />
+            ),
+          },
+        ]}
         dataSource={getProfessionalsDataSource(professionals)}
         pagination={false}
       />
+      <Modal
+        title="Title"
+        open={isModalOpen}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <CreateProfessionalForm
+          form={form}
+          defaultValues={professionals[selectedProfessional.current?.index!]}
+        />
+      </Modal>
     </div>
   );
 }
