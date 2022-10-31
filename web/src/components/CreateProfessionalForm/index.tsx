4e95b59296832034b362c17b9a2acfa8c82c8692
Arthur Almeida <arthursa.dev@gmail.com>
2022-10-30 22:23:12 -0300

    feat: shows an alert if there isn't any professional type registered and disables form

--- web/src/components/CreateProfessionalForm/index.tsx
+++ web/src/components/CreateProfessionalForm/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Form, FormInstance, Input, Select, Switch } from "antd";
+import { Alert, Form, FormInstance, Input, Select, Switch } from "antd";
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
 import { getProfessionalsType } from "../../services/getProfessionalsType";
@@ -34,76 +34,87 @@ export function CreateProfessionalForm({ form, defaultValues }: CreateProfession
   }, [defaultValues]);
   
   return (
-    <Form
-      layout="vertical"
-      form={form}
-      initialValues={{ situation: false }}
-    >
-      <Form.Item 
-        label="Nome" 
-        name="name"
-        required 
-        rules={[{ message: 'Digite o nome', required: true }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Telefone"
-        name="phone"
-        required 
-        rules={[{
-          message: 'Digite o telefone',
-          required: true,
-          len: 14
-        }]}
-        normalize={(value) => (
-          value
-            .replace(/\D/g, '')
-            .replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3')
-        )}
-      >
-        <Input placeholder="Somente números" maxLength={14} />
-      </Form.Item>
-      <Form.Item
-        label="E-mail"
-        name="email"
-        required 
-        rules={[{ message: 'Digite o endereço de e-mail', required: true, type: 'email' }]}
-      >
-        <Input placeholder="(99)99999-9999" />
-      </Form.Item>
-      <Form.Item
-        label="Cargo"
-        name="professionalType"
-        required 
-        rules={[{ message: 'Selecione um cargo', required: true }]}
-      >
-        <Select
-          placeholder="Selecione um cargo"
-        >
-          {professionalsType.map((professionalType) => (
-            <Option
-              key={professionalType.id}
-              value={professionalType.id}
-            >
-              {professionalType.description}
-            </Option>
-          ))}
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="Situação"
-        name="situation"
-        required 
-        rules={[{ message: 'Selecione a situação', required: true }]}
-        valuePropName="checked"
-      >
-        <Switch
-          checkedChildren={<CheckOutlined />}
-          unCheckedChildren={<CloseOutlined />}
-          defaultChecked={false}
+    <div>
+      {!professionalsType.length && (
+        <Alert
+          message="Informação"
+          description="Antes de cadastrar um novo profissional, certifique-se de cadastrar antes o tipo do professional"
+          type="warning"
+          showIcon
         />
-      </Form.Item>
-    </Form>
+      )}
+      <Form
+        disabled={!professionalsType.length}
+        layout="vertical"
+        form={form}
+        initialValues={{ situation: false }}
+      >
+        <Form.Item
+          label="Nome"
+          name="name"
+          required
+          rules={[{ message: 'Digite o nome', required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Telefone"
+          name="phone"
+          required
+          rules={[{
+            message: 'Digite o telefone',
+            required: true,
+            len: 14
+          }]}
+          normalize={(value) => (
+            value
+              .replace(/\D/g, '')
+              .replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3')
+          )}
+        >
+          <Input placeholder="Somente números" maxLength={14} />
+        </Form.Item>
+        <Form.Item
+          label="E-mail"
+          name="email"
+          required
+          rules={[{ message: 'Digite o endereço de e-mail', required: true, type: 'email' }]}
+        >
+          <Input placeholder="(99)99999-9999" />
+        </Form.Item>
+        <Form.Item
+          label="Cargo"
+          name="professionalType"
+          required
+          rules={[{ message: 'Selecione um cargo', required: true }]}
+        >
+          <Select
+            placeholder="Selecione um cargo"
+          >
+            {professionalsType.map((professionalType) => (
+              <Option
+                key={professionalType.id}
+                value={professionalType.id}
+              >
+                {professionalType.description}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Situação"
+          name="situation"
+          required
+          rules={[{ message: 'Selecione a situação', required: true }]}
+          valuePropName="checked"
+        >
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            defaultChecked={false}
+          />
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
